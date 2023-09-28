b6bc3634ac98e00b0784f1054b4d1189bad99d73
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-09-28 12:40:30 -0700

    read and displayed the contents of an existing text file, such as sample.txt.

--- task2.py
+++ task2.py
@@ -1,5 +1,13 @@
 # exersice>>1
 
-with open('my_file.txt', 'w') as f:
-    f.write('first file handling in python\n')
-    f.write('second string 56rdx#$%\n')
+# with open('my_file.txt', 'w') as f:
+#     f.write('first file handling in python\n')
+#     f.write('second string 56rdx#$%\n')
+
+
+# exersice>>2
+
+# with open('sample.txt', 'r') as f:
+#     file_contents = f.read()
+#
+# print(file_contents)
