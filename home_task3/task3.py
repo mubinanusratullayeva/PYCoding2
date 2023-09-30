0cee945587cec91cee64d004aa1ebfbb98312a75
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-09-30 12:11:13 -0700

    first task has just done

--- home_task3/task3.py
+++ home_task3/task3.py
@@ -2,3 +2,39 @@
 
 # task-1
 
+# init_name = input('Enter your name: ')
+# init_color = input('Enter the color of your favorite fruit from the colors of the rainbow: ')
+# class Fruit:
+#     fruits = {
+#         'orange':'orange',
+#         'yellow':'lemon',
+#         'green':'apple',
+#         'red':'strawberry',
+#         'pink':'raspberry',
+#         'purple':'plum',
+#         'blue':'currant'
+#     }
+#
+# class Check_input:
+#     def __init__(self, name, favo_color, obj):
+#         self.name = name
+#         self.favo_color = favo_color
+#         self.obj = obj
+#     def checking(self):
+#         if self.favo_color in self.obj.keys():
+#             check_l = input(f'{self.name}, do you like this {self.obj[self.favo_color]}? yes/no:  ')
+#             if check_l == 'yes':
+#                 return 'Great😊'
+#             else:
+#                 return 'Tragic😔'
+#         else:
+#             return 'Oh no, there is no fruit that you want'
+#
+# return_to_check = Check_input(init_name, init_color, Fruit.fruits)
+#
+# print(return_to_check.checking())
+
+
+
+
+
